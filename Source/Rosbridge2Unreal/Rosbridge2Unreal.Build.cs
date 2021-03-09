020797ce98334e093ef951be9417312fb882a170
Sebastian Pape <pape@vr.rwth-aachen.de>
2021-03-09 12:22:21 +0100

    Unifying tabs/spaces

--- Source/Rosbridge2Unreal/Rosbridge2Unreal.Build.cs
+++ Source/Rosbridge2Unreal/Rosbridge2Unreal.Build.cs
@@ -10,9 +10,9 @@ public class Rosbridge2Unreal : ModuleRules
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDefinitions.AddRange(
-            new string[]{}
-            );
+		PublicDefinitions.AddRange(
+			new string[]{}
+			);
 		
 		PublicDependencyModuleNames.AddRange(
 			new string[]
@@ -26,20 +26,20 @@ public class Rosbridge2Unreal : ModuleRules
 				"jsoncons"
 			}
 			);
-        
-        #if UE_4_26_OR_LATER
-        PublicDependencyModuleNames.Add("DeveloperSettings");
-        #endif
+		
+		#if UE_4_26_OR_LATER
+		PublicDependencyModuleNames.Add("DeveloperSettings");
+		#endif
 		
 		PrivateDependencyModuleNames.AddRange(
 			new string[]{}
 			);
 
 
-        DynamicallyLoadedModuleNames.AddRange(
-            new string[]{}
-        );
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]{}
+		);
 
-        AddEngineThirdPartyPrivateStaticDependencies(Target, "OpenSSL");
-    }
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "OpenSSL");
+	}
 }
