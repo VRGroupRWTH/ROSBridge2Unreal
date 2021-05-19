0d000c17fa71ebc541c1e4ad880b0bd1e701d7ce
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2021-05-19 13:23:49 +0200

    Revert changes in TCPConnection

--- Source/Rosbridge2Unreal/Private/Socket/TCPConnection.cpp
+++ Source/Rosbridge2Unreal/Private/Socket/TCPConnection.cpp
@@ -9,14 +9,14 @@
 bool UTCPConnection::Initialize(FString IPAddress, int Port, ETransportMode Mode)
 {
 	Socket = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(NAME_Stream, TEXT("Rosbridge TCP client"), false);
-	
+
 	TSharedRef<FInternetAddr> InternetAddress = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateInternetAddr();
 	bool bIPValid;
 	InternetAddress->SetIp(*IPAddress, bIPValid);
 	InternetAddress->SetPort(Port);
 
 	Socket->SetReceiveBufferSize(65536, ReceiveBufferSize); //16bit address, used by ROSBridge as a default
-	
+
 	if (!bIPValid)
 	{
 		UE_LOG(LogROSBridge, Error, TEXT("Given IP address is invalid: %s"), *IPAddress);
@@ -28,9 +28,9 @@ bool UTCPConnection::Initialize(FString IPAddress, int Port, ETransportMode Mode
 		UE_LOG(LogROSBridge, Error, TEXT("Could not connect to: %s:%d"), *IPAddress, Port);
 		return false;
 	}
-	
+
 	CurrentTransportMode = Mode;
-	
+
 	// Setting up and starting the receiver thread
 	ReceiverThread = FRunnableThread::Create(this, TEXT("ROSBridgeReceiverThread"), 0, TPri_Normal);
 
@@ -40,18 +40,16 @@ bool UTCPConnection::Initialize(FString IPAddress, int Port, ETransportMode Mode
 void UTCPConnection::Uninitialize()
 {
 	bTerminateReceiverThread = true;
-	if (ReceiverThread)
-	{
+	if (ReceiverThread) {
 		ReceiverThread->WaitForCompletion();
 	}
-	if (Socket)
-	{
+	if (Socket) {
 		Socket->Close();
 		ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->DestroySocket(Socket);
 	}
 }
 
-bool UTCPConnection::SendMessage(const uint8_t *Data, const unsigned int Length) const
+bool UTCPConnection::SendMessage(const uint8_t* Data, const unsigned int Length) const
 {
 	int32 BytesSend = 0;
 	return Socket->Send(Data, Length, BytesSend) && BytesSend == Length;
@@ -61,24 +59,22 @@ bool UTCPConnection::SendMessage(const uint8_t *Data, const unsigned int Length)
 uint32 UTCPConnection::Run()
 {
 	bReceiverThreadRunning = true;
-	
+
 	TArray<uint8> BinaryBuffer;
 	BinaryBuffer.Reserve(ReceiveBufferSize); // at least accomodate the whole socket buffer
 	uint32 PendingData = 0;
 	jsoncons::json_decoder<jsoncons::ojson> Decoder;
 	jsoncons::json_parser Parser;
 
-	while (!bTerminateReceiverThread)
-	{
+	while (!bTerminateReceiverThread) {
 
 		//Check Connection State
 		const ESocketConnectionState ConnectionState = Socket->GetConnectionState();
-		if (ConnectionState == SCS_NotConnected)
-		{
+		if (ConnectionState == SCS_NotConnected) {
 			FPlatformProcess::Sleep(0.5);
 			continue;
 		}
-		else if (ConnectionState == SCS_ConnectionError){
+		else if (ConnectionState == SCS_ConnectionError) {
 			UE_LOG(LogROSBridge, Error, TEXT("Error on connection"));
 			ReportError(ETransportError::SocketError);
 			bReceiverThreadRunning = false;
@@ -90,8 +86,7 @@ uint32 UTCPConnection::Run()
 			continue; // check if any errors occured
 		}
 
-		if
-		(CurrentTransportMode == ETransportMode::BSON){
+		if (CurrentTransportMode == ETransportMode::BSON) {
 			if (!Socket->HasPendingData(PendingData) || PendingData < 4)
 			{
 				continue; //wait further
@@ -108,12 +103,9 @@ uint32 UTCPConnection::Run()
 			}
 
 			//Initialize Buffer
-			if (BinaryBuffer.Num() < BSONMessageLength)
-			{
-				BinaryBuffer.SetNumZeroed(BSONMessageLength, false);
-			}
-			FMemory::Memcpy(BinaryBuffer.GetData(),&BSONMessageLength,sizeof(BSONMessageLength));
-				
+			if (BinaryBuffer.Num() < BSONMessageLength) BinaryBuffer.SetNumZeroed(BSONMessageLength, false);
+			FMemory::Memcpy(BinaryBuffer.GetData(), &BSONMessageLength, sizeof(BSONMessageLength));
+
 			//Receive BSONMessageLength bytes in binary_buffer
 			int32 TotalBytesRead = sizeof(BSONMessageLength);
 			while (TotalBytesRead < BSONMessageLength)
@@ -129,32 +121,27 @@ uint32 UTCPConnection::Run()
 			}
 
 			ROSData Data;
-			try
-			{
+			try {
 				Data = jsoncons::bson::decode_bson<ROSData>(BinaryBuffer.GetData(), BinaryBuffer.GetData() + BSONMessageLength);
 			}
-			catch(jsoncons::ser_error e)
+			catch (jsoncons::ser_error e)
 			{
 				UE_LOG(LogROSBridge, Error, TEXT("Error while parsing BSON message (Ignoring message)"));
 				continue;
 			}
-			
-			if (IncomingMessageCallback && !bTerminateReceiverThread)
-			{
-				IncomingMessageCallback(Data);
-			}
-			
+
+			if (IncomingMessageCallback && !bTerminateReceiverThread) IncomingMessageCallback(Data);
+
 		}
-		else if (CurrentTransportMode == ETransportMode::JSON)
-		{
+		else if (CurrentTransportMode == ETransportMode::JSON) {
 
 			if (!Socket->HasPendingData(PendingData)) continue;
-			
+
 			//Ensure space in buffer
-			if(static_cast<uint32>(BinaryBuffer.Num()) < PendingData) BinaryBuffer.SetNumUninitialized(PendingData, false);
-			
+			if (static_cast<uint32>(BinaryBuffer.Num()) < PendingData) BinaryBuffer.SetNumUninitialized(PendingData, false);
+
 			int32 BytesRead = 0;
-			if(!Socket->Recv(BinaryBuffer.GetData(), PendingData, BytesRead))
+			if (!Socket->Recv(BinaryBuffer.GetData(), PendingData, BytesRead))
 			{
 				UE_LOG(LogROSBridge, Error, TEXT("Failed to receive from socket. Closing receiver thread."));
 				ReportError(ETransportError::SocketError);
@@ -164,22 +151,28 @@ uint32 UTCPConnection::Run()
 
 			if (BytesRead <= 0) continue;
 
-			try{
+			try {
 				Parser.update(reinterpret_cast<char*>(BinaryBuffer.GetData()), BytesRead);
 				do
 				{
 					Parser.parse_some(Decoder);
-					
-					if(Parser.done())
+
+					if (Parser.done())
 					{
 						ROSData Data = Decoder.get_result();
 						if (IncomingMessageCallback && !bTerminateReceiverThread) IncomingMessageCallback(Data);
 						Parser.reset();
 					}
-				} while(!Parser.source_exhausted());
-			}catch(jsoncons::ser_error e)
+				} while (!Parser.source_exhausted());
+			}
+			catch (jsoncons::ser_error e)
 			{
 				UE_LOG(LogROSBridge, Fatal, TEXT("Error while parsing JSON message: %hs"), e.what());
 				throw;
 			}
 		}
+	}
+
+	bReceiverThreadRunning = false;
+	return EXIT_SUCCESS;
+}
